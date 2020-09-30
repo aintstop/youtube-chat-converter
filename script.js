8b9fb752132a36be2ea3d75f4b0f33beada12137
Glitch (hello-webpage) <none>
2020-09-30 10:32:31 +0000

    👩‍👩‍👦🏋 Checkpoint ./index.html:15859350/6747 ./script.js:15859350/373

--- script.js
+++ script.js
@@ -4,16 +4,20 @@ $(() => {
 
 function processText () {
   let originalText = $('#tOriginal').val()
-  let step1 = originalText.replace(/\n\n/gm, 'XXX')
+  let step1 = originalText.replace(/\n\n/gm, 'XXXXXXXX')
   let step2 = step1.replace(/\n/gm, ' — ')
-  let step3 = step2.replace(/XXX/gm, '\n\n')
+  let step3 = step2.split('XXXXXXXX')
   
-  let matches = [...step3.matchAll(/ — ([\s\S]*?) — /gm)]
+  let matches = [...step3.join('').matchAll(/ — ([\s\S]*?) — /gm)]
   let allNames = matches.map(item => item[1][0].toUpperCase() + item[1].substring(1))
   let uniqueNames = allNames.filter((value, index, self) => self.indexOf(value) === index)
   let sortedNames = uniqueNames.sort()
   
-  $('#tProcessed').val(step3)
-  $('#tViewers').val(sortedNames.join('\n'))  
+  $('#lProcessed').html(`Texto a ser salvo no Google Drive: [<b>${step3.length} Comentário(s)</b>]`)
+  $('#tProcessed').val(step3.join('\n\n'))
+  
+  $('#lViewers').html(`Listagem de espectadores em ordem alfabética: [<b>${sortedNames.length} Pessoa(s)</b>]`)
+  $('#tViewers').val(sortedNames.join('\n'))
+  
   $('#dResults').addClass('d-block').removeClass('d-none')
 }
