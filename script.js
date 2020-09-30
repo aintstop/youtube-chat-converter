04c394c092de3695d1255dcbd1ff0769436bde3c
Glitch (hello-webpage) <none>
2020-09-30 10:03:50 +0000

    🏣🕴 Checkpoint ./script.js:15859350/417 ./index.html:15859350/156

--- script.js
+++ script.js
@@ -2,10 +2,15 @@
     to your site with Javascript */
 
 // prints "hi" in the browser's dev tools console
+
 $(() => {
   $('#bProcess').click(processText)
 })
 
 function processText () {
-  let originalText = $('#tOriginalText').value()
+  let originalText = $('#tOriginal').val()
+  let step1 = originalText.replace(/\n\n/gm, 'XXX')
+  let step2 = step1.replace(/\n/gm, ' — ')
+  let step3 = step2.replace(/XXX/gm, '\n\n')
+  $('#tProcessed').val(step3)
 }
